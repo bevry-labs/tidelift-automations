78ce8870380bf092d2ce6a75bd4567add857a510
Benjamin Lupton <b@lupton.cc>
2023-11-15 14:41:39 +0800

    Update automate-release-managers.js

--- source/automate-release-managers.js
+++ source/automate-release-managers.js
@@ -1,24 +1,25 @@
 function waitSeconds (seconds) {
 	return new Promise(resolve => setTimeout(resolve, seconds * 1000))
 }
-async function verifyReleaseManagers () {
+async function verifyReleaseManagers (index = 0) {
 	document.title = 'waiting for packages'
 	while ( ! document.querySelector('div.small-padding.clickable-cursor') ) {
 		await waitSeconds(1) // still loading
 	}
 	
 	document.title = 'finding package'
-	const package = document.querySelector('div.small-padding.clickable-cursor[task_count="1"]')
-	if ( !package ) {
+	const $package = document.querySelectorAll('div.small-padding.clickable-cursor[task_count="1"]')[index]
+	if ( !$package ) {
 		document.title = 'automation complete'
 		alert(document.title)
 		return
 	}
-	package.click()
+	$package.click()
 	
 	document.title = 'finding maintainers'
-	let managers = '', $managers, $checkbox, $submit
+	let managers = '', $managers, $checkbox, $submit, $background
 	while ( !managers || !$managers || !$checkbox || !$submit ) {
+		$background = document.querySelector('.modal-background')
 		$submit = document.querySelector('.button.is-primary')
 		$checkbox = document.querySelector('input[type=checkbox]')
 		$managers = document.querySelectorAll('.has-text-weight-bold.my-2')
@@ -26,9 +27,11 @@ async function verifyReleaseManagers () {
 		await waitSeconds(1)
 	}
 	if ( managers !== 'bevryme' ) {
-		document.title = 'Release managers are not bevryme: ' + managers
-		alert(document.title)
-		return
+		document.title = 'unexpected maintainers: ' + managers
+		$package.innerText += ': ' + managers
+		$background.click()
+		await waitSeconds(1)
+		return verifyReleaseManagers(index + 1)
 	}
 	
 	document.title = 'confirming maintainers'
@@ -40,6 +43,6 @@ async function verifyReleaseManagers () {
 	await waitSeconds(1)
 	
 	document.title = 'continuing'
-	return verifyReleaseManagers()
+	return verifyReleaseManagers(index)
 }
 verifyReleaseManagers()
