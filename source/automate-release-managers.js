1245d4c4a2fb0db85a5b36b937730d7530fd28c5
Benjamin Lupton <b@lupton.cc>
2023-11-15 14:28:51 +0800

    Update automate-release-managers.js

--- source/automate-release-managers.js
+++ source/automate-release-managers.js
@@ -1,26 +1,42 @@
-function wait (ms) {
-	return new Promise(resolve => setTimeout(resolve, ms))
+function waitSeconds (seconds) {
+	return new Promise(resolve => setTimeout(resolve, seconds * 1000))
 }
 async function verifyReleaseManagers () {
-	document.title = 'automating'
-	if ( ! document.querySelector('div.small-padding.clickable-cursor') ) {
-		await wait(1000) // still loading
-		return verifyReleaseManagers()
+	document.title = 'waiting for packages'
+	while ( ! document.querySelector('div.small-padding.clickable-cursor') ) {
+		await waitSeconds(1) // still loading
 	}
+	
+	document.title = 'finding package'
 	const package = document.querySelector('div.small-padding.clickable-cursor[task_count="1"]')
-	if ( !package ) return alert('all done')
+	if ( !package ) {
+		document.title = 'automation complete'
+		alert(document.title)
+		return
+	}
 	package.click()
+	
+	document.title = 'finding maintainers'
 	let managers = ''
 	while ( !managers ) {
 		managers = Array.from(document.querySelectorAll('.has-text-weight-bold.my-2') || []).map(i => i.innerText).join(', ')
-		await wait(1000)
+		await waitSeconds(1)
 	}
-	if ( managers !== 'bevryme' ) return alert('Release managers are not bevryme: ' + managers)
-	await wait(1000)
+	if ( managers !== 'bevryme' ) {
+		document.title = 'Release managers are not bevryme: ' + managers
+		alert(document.title)
+		return
+	}
+	
+	document.title = 'confirming maintainers'
 	document.querySelector('input[type=checkbox]').click()
-	await wait(1000)
+	await waitSeconds(1)
+	
+	document.title = 'saving maintainers'
 	document.querySelector('.button.is-primary').click()
-	await wait(1000)
+	await waitSeconds(1)
+	
+	document.title = 'continuing'
 	return verifyReleaseManagers()
 }
 verifyReleaseManagers()
